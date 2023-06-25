7beaeac0e845f1c14c4d723a33c2340d6116bab0
bemartin32 <117478587+bemartin32@users.noreply.github.com>
2023-06-25 14:17:37 -0700

    Add files via upload

---
+++ servers.test.js
@@ -0,0 +1,26 @@
+describe("Servers test (with setup and tear-down)", function () {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = "Alice";
+  });
+
+  it("should add a new server to allServers on submitServerInfo()", function () {
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers["server" + serverId].serverName).toEqual("Alice");
+  });
+
+  it("should not add a new server on submitServerInfo()", function () {
+    serverNameInput.value = "";
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  afterEach(function () {
+    serverId = 0;
+    allServers = {};
+    serverTbody.innerHTML = " ";
+  });
+});
